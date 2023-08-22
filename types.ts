e231bb6bdbee7858a72933f91105d67689fdf19f
Christopher Brown <christopher.brown@healthstream.com>
2023-08-22 15:40:10 -0500

    type annotations

---
+++ types.ts
@@ -0,0 +1,37 @@
+// Types in typeScript
+
+// -- Primitive types --
+// Number  Null        Any
+// String  Undefinded  Never
+// Boolean Void        Unknown
+
+// -- Object types --
+// Object   Tuple
+// Array    Enum
+// Funtion  !Others
+
+// Where do we apply TypeScript in out code?
+
+// Variable Types
+// - To assign a basic type to a we add :Type after the varible name
+// ex
+const movieTitle: string = "No country for old men";
+const age: number = 34;
+const isActive: boolean = true;
+
+// ^ This is also call Type Annotation
+
+// Strings
+// Strings represent character values "This is a string value"
+// We can tell TypeScript that something is a string using the
+// type annotation of string (all lowercase)
+
+// Numbers
+// Some programming lauguages have many number types.
+// flost, int etc.
+// In typeScript (as well as JavaScript) numbers are just numbers.
+// Numbers can be typed with a simple TYpe Annotation of number (all lowercase)
+
+// Booleans
+// Boolean variables represent simple true or false values
+// Booleans can be types with simple type annotation of boolean (all lowercase)
---
+++ variables.ts
@@ -0,0 +1,11 @@
+// Strings
+let movieTitle_2: string = "No country for old men";
+movieTitle_2 = "The Lost Boys";
+
+// Numbers
+let gameSaves: number = 20;
+gameSaves += 2;
+
+// Boolean
+let active: boolean = false;
+active = true;
