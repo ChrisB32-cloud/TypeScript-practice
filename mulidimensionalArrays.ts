b3317f7d9cf146363648a1e747ade9e15ba12035
Christopher Brown <christopher.brown@healthstream.com>
2023-09-14 16:26:11 -0500

    multidimensional arrays

---
+++ mulidimensionalArrays.ts
@@ -0,0 +1,19 @@
+// -- MULTIDIMENTIONAL ARRAYS --
+
+//
+
+// ex of a array with nested a nested array of strings
+const ticTakTow: string[][] = [
+  ["X", "O", "X"],
+  ["X", "X", "X"],
+  ["X", "O", "O"],
+  ["O", "O", "X"],
+];
+
+// ex of a triple nested array with numbers
+const numberArray: number[][][] = [
+  [
+    [12, 4, 112, 1, 23412, 41, 12, 3],
+    [23412, 34124, 124, 4421, 12],
+  ],
+];
