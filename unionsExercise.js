1a60496b8a73ae242d2e2bbccc23b8ba0d107bf8
Christopher Brown <christopher.brown@healthstream.com>
2023-09-22 15:34:10 -0500

    unions exercise

---
+++ unionsExercise.js
@@ -0,0 +1,35 @@
+// **********************************************
+// ******************* PART 1 *******************
+// **********************************************
+// Create a variable called highScore that can be a number OR a boolean
+var highScore = 0;
+highScore = true;
+// **********************************************
+// ******************* PART 2 *******************
+// **********************************************
+// create an array called stuff
+// it can be an array of numbers OR an array of strings
+// it cannot be an array of numbers and strings (mixed together)
+var AlotOfStuff = [1, 2, 3, "4", 5, 6, 7, "8"];
+// Create an array called colors that can hold a mixture of RGB and HSL color types
+var myColors = [
+    { r: 234, g: 543, b: 432 },
+    { h: 23343.34, s: 432343.23, l: 239423.98 },
+];
+// **********************************************
+// ******************* PART 6 *******************
+// **********************************************
+// Write a function called greet that accepts a single string OR an array of strings
+// It should print "Hello, <name>" for that single person OR greet each person in the array with the same format
+function greet(name) {
+    if (typeof name === "string") {
+        return "Hello ".concat(name);
+    }
+    else {
+        return "Hello ".concat(name[0]);
+    }
+}
+var checkFunc = greet("Clark");
+var checkFunc_2 = greet(["Lex"]);
+console.log(checkFunc);
+console.log(checkFunc_2);
---
+++ unionsExercise.ts
@@ -0,0 +1,91 @@
+// **********************************************
+// ******************* PART 1 *******************
+// **********************************************
+// Create a variable called highScore that can be a number OR a boolean
+
+let highScore: number | boolean = 0;
+highScore = true;
+
+// **********************************************
+// ******************* PART 2 *******************
+// **********************************************
+// create an array called stuff
+// it can be an array of numbers OR an array of strings
+// it cannot be an array of numbers and strings (mixed together)
+
+const AlotOfStuff: (number | string)[] = [1, 2, 3, "4", 5, 6, 7, "8"];
+
+// **********************************************
+// ******************* PART 3 *******************
+// **********************************************
+// Create a literal type called SkillLevel
+// There are 4 allowed values: "Beginner", "Intermediate", "Advanced", and "Expert"
+
+type SkillLevel = "Beginner" | "Intermediate" | "Advanced" | "Expert";
+
+// **********************************************
+// ******************* PART 4 *******************
+// **********************************************
+// Create a type called SkiSchoolStudent
+// name must be a string
+// age must be a number
+// sport must be "ski" or "snowboard"
+// level must be a value from the SkillLevel type (from above)
+
+type SkiSchoolStudent = {
+  name: string;
+  age: number;
+  sport: "ski" | "snowboard";
+  level: "Intermediate";
+};
+
+// **********************************************
+// ******************* PART 5 *******************
+// **********************************************
+// Define a type to represent an RGB color
+// r should be a number
+// g should be a number
+// b should be a number
+
+type RGB = {
+  r: number;
+  g: number;
+  b: number;
+};
+
+// Define a type to represent an HSL color
+// h should be a number
+// s should be a number
+// l should be a number
+
+type HSL = {
+  h: number;
+  s: number;
+  l: number;
+};
+
+// Create an array called colors that can hold a mixture of RGB and HSL color types
+
+const myColors: (RGB | HSL)[] = [
+  { r: 234, g: 543, b: 432 },
+  { h: 23343.34, s: 432343.23, l: 239423.98 },
+];
+
+// **********************************************
+// ******************* PART 6 *******************
+// **********************************************
+// Write a function called greet that accepts a single string OR an array of strings
+// It should print "Hello, <name>" for that single person OR greet each person in the array with the same format
+function greet(name: string | string[]) {
+  if (typeof name === "string") {
+    return `Hello ${name}`;
+  } else {
+    return `Hello ${name[0]}`;
+  }
+}
+
+const checkFunc = greet("Clark");
+const checkFunc_2 = greet(["Lex"]);
+
+console.log(checkFunc);
+console.log(checkFunc_2);
