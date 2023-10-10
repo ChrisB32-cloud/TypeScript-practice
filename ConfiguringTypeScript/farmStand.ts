36d01ac590e76a775d52c9b2a4b6c198074bdc1c
Christopher Brown <christopher.brown@healthstream.com>
2023-10-10 16:16:07 -0500

    files compiler options

--- ConfiguringTypeScript/farmStand.ts
+++ ConfiguringTypeScript/farmStand.ts
@@ -13,3 +13,9 @@ const printProduct = (product: Products): void => {
 
 // same applies to tsc -w, command will work for
 // files in the directory
+
+// We can tell typescript specific files we want it to
+// compile by adding files to our ts.config.json file
+// adding these files to the array will allow typescript to
+// compile those files and ignore the others that are
+// not in the list
